7fbea3f38f2f0fc4440dffa2f6e923c42fc643c1
Brian <brianbasabee@gmail.com>
2025-07-07 19:56:33 -0300

    Fix:Arreglos en diseño del sweetalert

--- src/assets/SweetAlert.js
+++ src/assets/SweetAlert.js
@@ -1,10 +1,20 @@
-import Swal from 'sweetalert2'
+import Swal from 'sweetalert2';
+import './sweetalert-marcial.css';
 
 export function dispararSweetBasico(titulo, text, icon, textoBoton) {
-    Swal.fire({
-        title: titulo,
-        text: text,
-        icon: icon,
-        confirmButtonText: textoBoton
-      })
+  Swal.fire({
+    title: titulo,
+    text: text,
+    icon: icon,
+    confirmButtonText: textoBoton,
+    background: '#1c1c1c',
+    color: '#f1f1f1',
+    iconColor: '#44A901', // Color del ícono
+    width: '600px',
+    customClass: {
+      title: 'titulo-marcial',
+      confirmButton: 'boton-marcial-amarillo',
+      popup: 'popup-marcial',
+    },
+  });
 }
